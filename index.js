465e2bca36c2fe33138d5128b3c4283c8595c15e
zhenyong25 <zhenyong25@gmail.com>
2022-10-09 16:38:11 +0800

    Added request token page

--- index.js
+++ index.js
@@ -7,6 +7,9 @@
 
 const ethereumButton = document.querySelector('.enableEthereumButton');
 const showAccount = document.querySelector('.showAccount');
+const showAccount1 = document.querySelector('.showAccount1'); 
+const showAccount2 = document.querySelector('.showAccount2'); 
+const showAccount3 = document.querySelector('.showAccount3');
 
 ethereumButton.addEventListener('click', () => {
   getAccount();
@@ -14,6 +17,14 @@ ethereumButton.addEventListener('click', () => {
 
 async function getAccount() {
   const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
   const account = accounts[0];
   showAccount.innerHTML = account;
+
+  const account1 = accounts[1];
+  showAccount1.innerHTML = account1;
+
+  const account2 = accounts[2];
+  showAccount2.innerHTML = account2;
+
 }
